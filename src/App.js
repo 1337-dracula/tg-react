c2a27a54fb88610d8df3ffcb466c9cd2a34485f1
1337-dracula <absurdvox@gmail.com>
2026-07-26 22:49:20 +0000

    new uptade 05

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import { useTelegram } from './hooks/useTelegram';
+import { Route, Routes } from 'react-router-dom'
+import Productlist from './components/Productlist/Productlist';
 
 
 function App() {
@@ -16,7 +18,10 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <button onClick={onToggleButton}>toggle</button>
+      <Routes>
+        <Route index element={<Productlist />} />
+        <Route path={'form'} element={<Form />} />
+      </Routes>
     </div>
   );
 }
