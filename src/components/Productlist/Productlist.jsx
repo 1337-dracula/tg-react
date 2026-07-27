23cfc26edb4822d80585ce4b8c31f8fcab223e1b
1337-dracula <absurdvox@gmail.com>
2026-07-27 16:02:11 +0000

    new uptade 09

--- src/components/Productlist/Productlist.jsx
+++ src/components/Productlist/Productlist.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useTelegram } from '../../hooks/useTelegram';
 import ProductItem from '../ProductionItem/ProductionItem';
 import './Productlist.css'
+import { useCallback, useEffect } from "react";
 
 
 const products = [
@@ -26,7 +27,29 @@ const getTotalPrice = (items) => {
 
 const Productlist = () => {
   const [addedItems, setAddedItems] = useState([]);
-  const { tg } = useTelegram();
+  const { tg, queryId } = useTelegram();
+
+  const onSendData = useCallback(() => {
+    const data = {
+      products: addedItems,
+      totalPrice: getTotalPrice(addedItems),
+      queryId,
+    }
+    fetch('http://100.84.64.11:8000/web-data', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+    })
+  }, [])
+
+  useEffect(() => {
+    tg.onEvent('mainButtonClicked', onSendData)
+    return () => {
+      tg.offEvent('mainButtonClicked', onSendData())
+    }
+  }, [onSendData])
 
   const onAdd = (product) => {
     const alreadyAdded = addedItems.find(item => item.id === product.id)
